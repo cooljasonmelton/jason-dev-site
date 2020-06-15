d490d5bd160a82e8d5ec1d5cba03c327f17d92c9
cooljasonmelton <cooljasonmelton>
2020-06-14 20:49:59 -0500

    logo

--- src/about/About.js
+++ src/about/About.js
@@ -5,6 +5,29 @@ import '../landing/Landing.css'
 const About = () => {
   return (
     <section className="spikes">
+      <div text-div>
+        <h1 className="title">About:</h1>
+        <div className="text-container">
+          <p className="about">
+            With a background in philosophy and comedy, I've noticed software developing has a similar underpinning. Whether analyzing a theorem, writing a joke, or creating an app, it's all about looking at logical consequences, research, and error correction.
+          </p>
+          <p className="about">
+            Coding is a recent upgrade to my life. I watched a friend switch to development and just how happy it made her. She suggested I try the same and I spent a year of diligent studying between shifts at a regular job. I immediately loved the creative and analytical nature of it so I took the plunge and fully committed. I entered and completed an intensive bootcamp at Flatiron School, and have now entered the job search confident in my hard-earned skills.
+          </p>
+          <p className="about">
+            I have an obsessive personality and enjoy tinkering with something until it's perfect. I love the creative, problem-solving nature of developing. I look forward to putting new stuff into the world.
+          </p>
+          <p className="about">
+            Languages: Javascript (React), Ruby on Rails.
+          </p>
+          <p className="about">
+            For fun, I enjoy chess and pro-wrestling.
+          </p>
+          <p className="about">
+            Founder of Root Beer Friday. #rootbeerfriday
+          </p>
+        </div>
+      </div>
 
        
     </section>
