8707e6755c7e963ad2153f6e68c204b143f72183
cooljasonmelton <cooljasonmelton>
2020-06-15 01:05:41 -0500

    blog css

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -8,22 +8,20 @@ import blog3 from '../images/blog-images/blog3.png'
 const Blog = () => {
   return (
     <section className="spikes">
-      <div className="main-div">
+      <div className="main-blog-div">
         <div className="title-container">
           <h1 className="title">Blog:</h1>
-          <h3 className="visit-blog">visit blog</h3>
+          <h3 className="visit-blog">visit blog on medium.com</h3>
         </div>
-        <div className="blog-div">
-          <a href="https://medium.com/@jasonmelton_/using-chart-js-to-create-tug-o-war-like-graphs-cb18324ec1a?source=friends_link&sk=15f23826c93f746175b4ad372191cb1d">
-            <img className="blog-pic" src={blog3} alt='blog'/>
-          </a>
-          <a href="https://medium.com/@jasonmelton_/i-figured-out-a-way-to-make-ruby-command-line-flip-books-and-i-did-this-by-learning-from-everyone-afe085635ff2?source=friends_link&sk=413ca43c4ecd2fa1c027f1e31e238dfb">
-            <img className="blog-pic" src={blog2} alt='blog'/>
-          </a>
-          <a href="https://medium.com/@jasonmelton_/using-rubys-succ-method-to-get-around-api-restrictions-1cdb47829d62?source=friends_link&sk=96b39694a8be7ab9be183416aaf9688d">
-            <img className="blog-pic" src={blog1} alt='blog'/>
-          </a>
-        </div>
+        <a className="blog-item" href="https://medium.com/@jasonmelton_/using-chart-js-to-create-tug-o-war-like-graphs-cb18324ec1a?source=friends_link&sk=15f23826c93f746175b4ad372191cb1d">
+          <img className="blog-pic" src={blog3} alt='blog'/>
+        </a>
+        <a className="blog-item" href="https://medium.com/@jasonmelton_/i-figured-out-a-way-to-make-ruby-command-line-flip-books-and-i-did-this-by-learning-from-everyone-afe085635ff2?source=friends_link&sk=413ca43c4ecd2fa1c027f1e31e238dfb">
+          <img className="blog-pic" src={blog2} alt='blog'/>
+        </a>
+        <a className="blog-item"href="https://medium.com/@jasonmelton_/using-rubys-succ-method-to-get-around-api-restrictions-1cdb47829d62?source=friends_link&sk=96b39694a8be7ab9be183416aaf9688d">
+          <img className="blog-pic" src={blog1} alt='blog'/>
+        </a>
       </div>
     </section>
   )
