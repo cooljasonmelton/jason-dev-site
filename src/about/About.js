2d948163848bed143b83e1cf4a5402aa164edb56
cooljasonmelton <cooljasonmelton>
2020-06-14 22:58:54 -0500

    contact

--- src/about/About.js
+++ src/about/About.js
@@ -1,35 +1,37 @@
 import React from 'react'
 import './About.css'
 import '../landing/Landing.css'
+import jasonpic from '../images/jasonpic.jpg'
 
 const About = () => {
   return (
     <section className="spikes">
-      <div text-div>
-        <h1 className="title">About:</h1>
-        <div className="text-container">
-          <p className="about">
-            With a background in philosophy and comedy, I've noticed software developing has a similar underpinning. Whether analyzing a theorem, writing a joke, or creating an app, it's all about looking at logical consequences, research, and error correction.
-          </p>
-          <p className="about">
-            Coding is a recent upgrade to my life. I watched a friend switch to development and just how happy it made her. She suggested I try the same and I spent a year of diligent studying between shifts at a regular job. I immediately loved the creative and analytical nature of it so I took the plunge and fully committed. I entered and completed an intensive bootcamp at Flatiron School, and have now entered the job search confident in my hard-earned skills.
-          </p>
-          <p className="about">
-            I have an obsessive personality and enjoy tinkering with something until it's perfect. I love the creative, problem-solving nature of developing. I look forward to putting new stuff into the world.
-          </p>
-          <p className="about">
-            Languages: Javascript (React), Ruby on Rails.
-          </p>
-          <p className="about">
-            For fun, I enjoy chess and pro-wrestling.
-          </p>
-          <p className="about">
-            Founder of Root Beer Friday. #rootbeerfriday
-          </p>
+      <div className="main-div">
+        <img src={jasonpic} className="pic"/>
+        <div className="text-div">
+          <div className="text-container">
+            <h1 className="title">About:</h1>
+            <p className="subtitle">
+              <b>Languages:</b> Javascript (React), Ruby on Rails.
+            </p>
+            <p className="subtitle">
+              <b>Bio:</b> With a background in philosophy and comedy, I've noticed software developing has a similar underpinning. Whether analyzing a theorem, writing a joke, or creating an app, it's all subtitle looking at logical consequences, research, and error correction.
+            </p>
+            <p className="subtitle">
+              Coding is a recent upgrade to my life. I watched a friend switch to development and just how happy it made her. She suggested I try the same and I spent a year of diligent studying between shifts at a regular job. I immediately loved the creative and analytical nature of it so I took the plunge and fully committed. I entered and completed an intensive bootcamp at Flatiron School, and have now entered the job search confident in my hard-earned skills.
+            </p>
+            <p className="subtitle">
+              I have an obsessive personality and enjoy tinkering with something until it's perfect. I love the creative, problem-solving nature of developing. I look forward to putting new stuff into the world.
+            </p>
+            <p className="subtitle">
+              For fun, I enjoy chess and pro-wrestling.
+            </p>
+            <p className="subtitle">
+              Founder of Root Beer Friday. #rootbeerfriday
+            </p>
+          </div>
         </div>
       </div>
-
-       
     </section>
   )
 }
--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,55 +1,72 @@
 import React from 'react'
 import '../landing/Landing.css'
 import './Contact.css'
-import jasonpic from '../images/jasonpic.jpg'
+import copyPic from '../images/copy.svg'
 
-const handleClick = (e, props) => {
-  console.log(props)
-  switch(e.target.textContent){
-    case 'jason.melton2@gmail.com':
-      window.location.href = 'mailto:jason.melton2@gmail.com'
-      break
-    case 'github.com/cooljasonmelton':
-      window.location.href = 'http://www.github.com/cooljasonmelton'
-      break
-    case '@captainjmelton':
-      window.location.href = 'https://twitter.com/CaptainJMelton'     
-      break
-    default: 
-      console.log('default')
+
+
+
+class Contact extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      copied: false
+    }
   }
- 
+
+  handleClick = (e) => {
+    switch(e.target.textContent){
+      case 'jason.melton2@gmail.com':
+        window.location.href = 'mailto:jason.melton2@gmail.com'
+        break
+      case 'github.com/cooljasonmelton':
+        window.location.href = 'http://www.github.com/cooljasonmelton'
+        break
+      case '@captainjmelton':
+        window.location.href = 'https://twitter.com/CaptainJMelton'     
+        break
+      default: 
+        console.log('default')
+    }
+  }
+  
+  handleCopy = e => {
+    navigator.clipboard.writeText('jason.melton2@gmail.com')
+    this.setState(prevState => ({ copied: !prevState.copied }));
+  }
 
 
-}
 
-
-const Contact = () => {
+  render(){
   return (
     <section className="spikes">
-      <div className='main-div'>
-        <div className="text-div">
-          <h1 className="title">Contact:</h1>
-          <h3 className="subtitle">email:</h3>
-          <h3 className="contact-info">
-            <u onClick={handleClick}>jason.melton2@gmail.com</u>
-          </h3>
-          <h3 className="subtitle">github:</h3>
-          <h3 className="contact-info">
-            <u onClick={handleClick}>github.com/cooljasonmelton</u>
-          </h3>
-          <h3 className="subtitle">twitter:</h3>
-          <h3 className="contact-info">              
-            <u onClick={handleClick}>@captainjmelton</u>
-          </h3>
-        </div>
-        <div>
-          <img className="pic" src={jasonpic} />
+      <div className="main-div">
+        <h1 className="title">Contact:</h1>
+        <div className="all-contacts">
+          <div className="contact-div">
+            <div className="copy-div">
+              <h2 className="contact-subtitle"><u>email:</u></h2>
+              <img src={copyPic} onClick={this.handleCopy} className="copyPic"/>
+            </div>
+            <h3 className="contact-info" onClick={this.handleClick}>jason.melton2@gmail.com</h3>
+            <p className={this.state.copied? "copy-message" : "display-off"}>copied to clipboard!</p>
+          </div>
+          <div className="contact-div">
+            <h2 className="contact-subtitle"><u>github:</u></h2>
+            <h3 className="contact-info" onClick={this.handleClick}>github.com/cooljasonmelton</h3>
+          </div>
+          <div className="contact-div">
+            <h2 className="contact-subtitle"><u>twitter:</u></h2>
+            <h3 className="contact-info" onClick={this.handleClick}>@captainjmelton</h3>
+          </div>
         </div>
       </div>
+      
 
-    </section>
-  )
+      </section>
+      
+    )
+  }
 }
 
 export default Contact
