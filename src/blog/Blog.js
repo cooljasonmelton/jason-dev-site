bdbe4e588a70982b6c91b0724c0f6406ef26bef9
cooljasonmelton <cooljasonmelton>
2020-06-17 02:49:39 -0500

    css touches

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -13,8 +13,10 @@ const Blog = () => {
           <h1 className="title">Blog:</h1>
           <h3 className="visit-blog">visit blog on medium.com</h3>
         </div>
-        <a className="blog-item" href="https://medium.com/@jasonmelton_/using-chart-js-to-create-tug-o-war-like-graphs-cb18324ec1a?source=friends_link&sk=15f23826c93f746175b4ad372191cb1d">
-          <img className="blog-pic" src={blog3} alt='blog'/>
+        <a href="https://medium.com/@jasonmelton_/using-chart-js-to-create-tug-o-war-like-graphs-cb18324ec1a?source=friends_link&sk=15f23826c93f746175b4ad372191cb1d">
+          <div className="blog-item">
+            <img className="blog-pic" src={blog3} alt='blog'/>
+          </div>
         </a>
         <a className="blog-item" href="https://medium.com/@jasonmelton_/i-figured-out-a-way-to-make-ruby-command-line-flip-books-and-i-did-this-by-learning-from-everyone-afe085635ff2?source=friends_link&sk=413ca43c4ecd2fa1c027f1e31e238dfb">
           <img className="blog-pic" src={blog2} alt='blog'/>
